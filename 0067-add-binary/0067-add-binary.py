08d6391c2ff6c5f8f19590ae4577642c40edad3f
Sushma Nandiyawar <49101635+Sushma-Nandiyawar@users.noreply.github.com>
2025-04-10 21:12:52 -0400

    Time: 2 ms (49.71%), Space: 17.8 MB (51.98%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,21 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        result = ""
+        a, b = a[::-1], b[::-1]
+        carry = 0
+        for i in range(max(len(a), len(b))):
+            da = ord(a[i]) - ord("0") if i<len(a) else 0
+            db = ord(b[i]) - ord("0") if i<len(b) else 0
+
+            total = da+ db+ carry
+            char = str(total%2)
+            result = char + result
+            carry = total // 2
+        if carry:
+            result = "1"+result
+        return result
+
+
+
+
+
