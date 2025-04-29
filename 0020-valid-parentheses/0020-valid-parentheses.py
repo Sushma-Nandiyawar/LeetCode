d6dce9aa54e18872e6e39cdc875a9576ca1a0c61
Sushma Nandiyawar <49101635+Sushma-Nandiyawar@users.noreply.github.com>
2025-04-29 18:36:10 +0530

    Time: 0 ms (100%), Space: 17.8 MB (81.15%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,32 +1,13 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        close_to_open = {
-            ")": "(",
-            "}": "{",
-            "]": "[",
-        }
-        stack = []
-
-        for brace in s:
-            
-            # If it's an opening brace, simply push it in our stack
-            if brace not in close_to_open:
-                stack.append(brace)
-            
-            # This means we have encountered a closing brace 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        hash = {']':'[', '}':'{', ')':'('}
+        for char in s:
+            if char in hash:
+                if stack and stack[-1] == hash[char]:
+                    stack.pop()
+                else:
+                    return False
             else:
-                
-                # Before doing anything, first let's grab the top element from our 
-                stack_top_element = stack.pop() if stack else "F"
-
-                # Now check if stack_top_element is a valid open brace 
-                if close_to_open[brace] != stack_top_element:
-                    return False 
-        
-        # It may happen you have some opening braces that weren't ever closed 
-
+                stack.append(char)
         return not stack
